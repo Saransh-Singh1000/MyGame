752eace8341798bebb9e0c19ea1465deedc72cec
Saransh-Singh1000 <saranshmanojsingh@gmail.com>
2026-05-21 15:32:56 +0530

    MyGame

--- Platform/Android/cpp/lib.cpp
+++ Platform/Android/cpp/lib.cpp
@@ -7,4 +7,7 @@ JNIEXPORT void JNICALL
 Java_org_main_MainActivity_nativeInit(JNIEnv* env, jobject thiz)
 {
     Log(LogLevel::Info, "Hello World");
+    Log(LogLevel::Warning, "Hello World");
+    Log(LogLevel::Error, "Hello World");
+    Log(LogLevel::Fatal, "Hello World");
 }
--- Platform/Windows/cpp/main.cpp
+++ Platform/Windows/cpp/main.cpp
@@ -6,5 +6,8 @@
 
 int main(){
     Log(LogLevel::Info, "Hello, World!" );
+    Log(LogLevel::Warning, "Hello World");
+    Log(LogLevel::Error, "Hello World");
+    Log(LogLevel::Fatal, "Hello World");
     return 0;
 }
