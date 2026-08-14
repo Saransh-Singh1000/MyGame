d54d81e93aa4d95ea258008b05809433b3aa60d6
Saransh-Singh1000 <saranshmanojsingh@gmail.com>
2026-08-14 08:18:00 +0530

    MyGame

--- Common/src/init/init.cpp
+++ Common/src/init/init.cpp
@@ -5,17 +5,18 @@
 namespace Engine {
 
 bool run() {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    // 1. SDL_Init returns true on success in SDL3
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
         std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return false;
     }
 
+    // 2. SDL_CreateWindow in SDL3 takes (title, width, height, flags)
+    // 3. SDL_WINDOW_SHOWN is no longer needed (windows are shown by default)
     SDL_Window* window = SDL_CreateWindow(
         "Engine Window",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
         1280, 720,
-        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
+        SDL_WINDOW_RESIZABLE
     );
 
     if (!window) {
@@ -30,7 +31,8 @@ bool run() {
     // Simple event loop
     while (isRunning) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+            // 4. SDL_QUIT is now SDL_EVENT_QUIT in SDL3
+            if (event.type == SDL_EVENT_QUIT) {
                 isRunning = false;
             }
         }
